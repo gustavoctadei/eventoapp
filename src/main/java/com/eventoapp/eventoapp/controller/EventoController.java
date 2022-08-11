362ccf21a55925819943869b309d48f4215237be
Gustavo Tadei <gustavo.tadei@hotmail.com>
2022-08-11 15:53:20 -0400

    Aula 02 - Ok

--- src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.eventoapp.eventoapp;
+package com.eventoapp.eventoapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  * @author Gustavo
  */
+
 @Controller
-public class IndexController {
+public class EventoController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+    @RequestMapping("/cadastrarEvento")
+    public String form() {
+        return "evento/formEvento";
     }
     
 }
---
+++ src/main/java/com/eventoapp/eventoapp/model/Evento.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eventoapp.eventoapp.model;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Evento {
+    
+    private String nome;
+    
+    private String local;
+    
+    private String data;
+    
+    private String horario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    
+}
