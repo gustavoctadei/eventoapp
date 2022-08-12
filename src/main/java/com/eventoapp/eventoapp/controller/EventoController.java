6da9659b48c9d7a22f09ca6982f90f5a55135355
Gustavo Tadei <gustavo.tadei@hotmail.com>
2022-08-12 11:53:37 -0400

    Aula 04 - Ok

--- src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
@@ -4,8 +4,12 @@
  */
 package com.eventoapp.eventoapp.controller;
 
+import com.eventoapp.eventoapp.dao.EventoDao;
+import com.eventoapp.eventoapp.model.Evento;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -15,9 +19,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EventoController {
     
-    @RequestMapping("/cadastrarEvento")
+    @Autowired
+    private EventoDao eventoDao;
+    
+    @RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
     public String form() {
         return "evento/formEvento";
+    }
+    
+    @RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+    public String form(Evento evento) {
+        eventoDao.save(evento);
+        
+        return "redirect:/cadastrarEvento";
     }
     
 }
---
+++ src/main/java/com/eventoapp/eventoapp/dao/EventoDao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.eventoapp.eventoapp.dao;
+
+import com.eventoapp.eventoapp.model.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Gustavo
+ */
+public interface EventoDao extends CrudRepository<Evento, String> {
+    
+}
